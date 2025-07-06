01b7287b0a3ebd4d1c9c0bf04fa5ae5c88bb3fcd
MediKings <89770307+MediKings@users.noreply.github.com>
2025-07-06 07:42:31 +0100

    Modification table property

--- properties/models.py
+++ properties/models.py
@@ -4,36 +4,43 @@ from accounts.models import CustomUser
 
 class Property(models.Model):
     PROPERTY_TYPES = [
-        ('House', 'Maison'),
-        ('Appartment', 'Appartement'),
-        ('Studio', 'studio'),
-        ('Villa', 'Villa'),
-        ('Land', 'Terrain'),
-        ('Commercial', 'Local commercial'),
+        ('maison', 'Maison'),
+        ('appartement', 'Appartement'),
+        ('studio', 'studio'),
+        ('villa', 'Villa'),
+        ('terrain', 'Terrain'),
+        ('commercial', 'Local commercial'),
     ]
 
     ANNONCE_TYPES = [
-        ('To_sell', 'À vendre'),
-        ('For_rent', 'À louer'),
+        ('À vendre', 'À vendre'),
+        ('À louer', 'À louer'),
     ]
 
     title = models.CharField(max_length=200)
     description = models.TextField()
     main_image = models.ImageField(upload_to='images/properties/')
+
+    price = models.PositiveIntegerField()
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
     property_type = models.CharField(max_length=20, choices=PROPERTY_TYPES)
     annonce_type = models.CharField(max_length=20, choices=ANNONCE_TYPES)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    area = models.DecimalField(max_digits=8, decimal_places=2)
+
+    long = models.IntegerField()
+    larg = models.IntegerField()
+
     bedrooms = models.IntegerField(default=0)
     bathrooms = models.IntegerField(default=0)
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
+    equipments = models.TextField()
+
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='properties')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.title} by {self.owner.id}'
+        return f'{self.title} by {self.owner}'
 
 class PropertyImage(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images')
